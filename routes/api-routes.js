322b2f55acba7a98a84bda703f3556bfd47bc496
Zachary Cohen <zacharycohen@Zacharys-MacBook-Pro.local>
2019-11-09 13:06:09 -0500

    routes working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,25 +12,56 @@ const db = require('../models');
 module.exports = function(app) {
   // GET route for getting all of the todos
   app.get('/api/todos', async (req, res) => {
-
+    const results = await db.Todo.findAll({});
+    res.json(results);
+    
   });
 
   // POST route for saving a new todo.
   // We can create a todo using the data on req.body
   app.post('/api/todos', async (req, res) => {
-
+    const result = await db.Todo.create({
+      text: req.body.text,
+      complete: req.body.complete,
+    });
+    res.json({id: result.insertId});
   });
 
   // DELETE route for deleting todos.
   // We can access the ID of the todo to delete in
   // req.params.id
   app.delete('/api/todos/:id', async (req, res) => {
-
+    const result = await db.Todo.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.json(result);
   });
 
   // PUT route for updating todos.
   // We can access the updated todo in req.body
-  app.put('/api/todos', async (req, res) => {
+//   app.put('/api/todos', async (req, res) => {
+//     const result = await db.Todo.update({
+//       where:{
+//         id: req.params.id,
+//       }
+//     });
+//     res.json(result)
 
+// };
+
+app.put('/api/todos', async (req, res) => {
+  const {id, text,complete} = req.body;
+  const result = await db.Todo.update(
+    {
+    text,
+    complete,
+  },
+  {
+    where: {id},
   });
+  res.send(result);
+
+});
 };
